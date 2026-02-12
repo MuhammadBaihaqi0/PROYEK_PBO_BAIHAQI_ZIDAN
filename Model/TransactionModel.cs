681d0430943a6fdd5f6763d6ec5bccbf69fe11e6
zidan hairra ramadhan <zidanramadhan950@gmail.com>
2026-02-12 09:44:53 +0700

    v

---
+++ Model/TransactionModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyek_besar_pbo_baihaqi_zidan.Model
+{
+    internal class TransactionModel
+    {
+    }
+}
